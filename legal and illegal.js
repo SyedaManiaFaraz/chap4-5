b586944025bc0b819c1fd91b80a44de09c667b79
SyedaManiaFaraz <syedamaniafaraz@gmail.com>
2024-08-27 23:51:30 +0500

    added index.html file

---
+++ legal and illegal.js
@@ -0,0 +1,22 @@
+// Question No 1
+var a = 2 , b = 4 , c = 6;
+
+
+
+// Question No 2
+// legal variable
+var num1 = 10;
+var a = "Mania";
+var variable$ = 30;
+var var123 = "Faraz";
+var variable_name = "hello";
+
+// illegal variable
+var #variable = 9;
+var 123var = "ahmed";
+var var-iable = 99;
+var name-variable = "shah";
+var %variable = 5;
+
+
+
---
+++ math.js
@@ -0,0 +1,92 @@
+// Question No 1 
+var message = " <h1> Question 01 </h1>\n Sum of 3 and 5 is "
+document.write(message)
+var num1 = 3;
+var num2 = 5;
+document.write(num1 + num2)
+
+
+// Question No 2
+var message = " <h1> Question 02 </h1>\n sub   =  "
+document.write(message)
+var num1 = 3;
+var num2 = 5;
+document.write( num1 - num2)
+
+var message = " <br> mult   =  "
+document.write(message)
+var num1 = 3;
+var num2 = 5;
+document.write( num1 *  num2)
+ 
+var message = " <br> div   =  "
+document.write(message)
+var num1 = 3;
+var num2 = 5;
+document.write( num1 /  num2)
+
+
+// Question No 3
+var message = " <h1> Question 03 </h1> \n value after variable declaration is undefined <br>Initial value:5 <br>value after increment is :6 <br> value after addition is :13 <br> value after decrement is : 12 <br> the remainder is :0"
+document.write(message) 
+
+// Question No 4
+var message = " <h1> Question 04 </h1> \n total cost to buy 5 tickets to movie is 3000PKR"
+document.write(message)
+
+// Question No 5
+var message = " <h1> Question 05</h1> \n 4*1=4 <br> 4*2=8 <br> 4*3=12 <br> 4*4=16 <br> 4*5=20 <br> 4*6=24 <br> 4*7=28 <br> 4*8=32 <br> 4*9=36 <br> 4*10=40"
+document.write(message)
+
+// Question No 6
+ var message = "<h1> Question 06</h1> \n 25<sup>"
+ document.write(message)
+
+// Question No 7
+var message="<h1> Question 07</h1> \n <h2>SHOPPING CART</h2>";
+document.write(messsage)
+
+var message="Price of item 1 is ";
+document.write(messsage)
+
+var item1=650;
+document.write(item1)
+
+var message="<br>Quantity of item 1 is ";
+document.write(messsage)
+
+var item1 = 3; 
+document.write (item1)
+
+var message="<br> Price of item 2 is";
+document.write(messsage)
+
+var item2 =100; 
+document.write(message)
+
+var message="<br> Quantity of item 2 is ";
+document.write(messsage)
+var item2=7
+document.write(item2)
+
+var message = "<br> <br> Total cost of order is "
+document.write(message)
+var total =2750;
+document.write(total)
+
+// Question No 08
+var message = "<h1>Question 08</h1> \n <h2> Marks Sheet</h2>";
+ document.write(message)
+
+ var message= "\n Total marks:900 <br> Marksobtained:804 ";
+ document.write(message)
+
+ var message ="\n Totalmarks:900 <br> Marksobtained:804 <br> Percentage:82.04 ";
+ document.write(message)
+
+// Question No 9
+var message= "<h1> Question 09</h1> \n <h2>Currency in PKR </h2>";
+document.write(message)
+
+var message= "Total Currency in PKR :1748";
+document.write(message)
